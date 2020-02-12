966651c635f69bf5a748d9eb17abce51d4fcd207
yoeho <lcdnd1996@gmail.com>
2020-02-12 13:15:39 +0630

    request change

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-  <a class="navbar-brand text-warning" href="{{url('products')}}">Store Management</a>
+  <a class="navbar-brand text-warning" href="{{url('/')}}">Store Management</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
